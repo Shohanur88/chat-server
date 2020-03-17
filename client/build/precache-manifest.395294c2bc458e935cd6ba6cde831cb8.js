402c8320b3c5caf2548f669a3c41c3d31b8f4de2
Shohanur88 <43010815+Shohanur88@users.noreply.github.com>
2020-03-16 23:21:39 -0700

    make it better

---
+++ client/build/precache-manifest.395294c2bc458e935cd6ba6cde831cb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b824ca9b4124f105ae9e469de87d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd93e53f53067ae1674f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "df73d3a08dcad9d9a829",
+    "url": "/static/css/main.6ee2020e.chunk.css"
+  },
+  {
+    "revision": "bd93e53f53067ae1674f",
+    "url": "/static/js/2.5e99f458.chunk.js"
+  },
+  {
+    "revision": "e2a595cebc19315c6e6690cf04f42e5b",
+    "url": "/static/js/2.5e99f458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df73d3a08dcad9d9a829",
+    "url": "/static/js/main.23560971.chunk.js"
+  },
+  {
+    "revision": "09cd4e33a876a871720e",
+    "url": "/static/js/runtime-main.ed1676f7.js"
+  }
+]);
---
+++ router.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const path = require('path');
+const session = require('express-session');
+
+const sql = require('./Sql');
+
+// let sessio ;
+
+// router.get('/chat', (req, res) =>{
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+router.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
+
+});
+
+router.get('/server',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
+
+});
+
+// router.post("/loginroom", (req, res) => {
+//    sessio = req.session;
+   
+//    sql.SqlConnect();
+//    const userData={
+//        username: req.body.username,
+//        password: req.body.password
+//    };
+//    sql.SqlLoginUser(userData,(check,result)=>{
+//        console.log(check);
+//        console.log(result);
+//        if(check === true){
+//             // session.username = result.username;
+//             // session.role = result.role;
+//             // session.uid = result.uid;
+//             // session.fname = result.fname;
+//        }
+//        res.send(result);
+//    })
+// //    sql.SqlLoginUser(userData,(check)=>{
+// //        console.log(check);
+// //    });
+// //    //console.log(userData);
+//    sql.SqlDisconnect();
+//    //res.send({Msg:"Its login page"});
+//    //console.log("rq accepted");
+// });
+
+// router.post("/registerblock", (req, res) => {
+//     //sessio = req.session;
+//     //sessio.uid = 100;
+//     sql.SqlConnect();
+//     const userData={
+//         fname: req.body.fname,
+//         username: req.body.username,
+//         password: req.body.password,
+//         role: req.body.role
+//     };
+//     sql.SqlInsertUser(userData,(e)=>{
+//         console.log(e);
+//     });
+//     console.log(userData);
+//     sql.SqlDisconnect();
+// });
+
+//
+
+module.exports = router
